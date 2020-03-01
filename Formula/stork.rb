f47c914a98c8f7c020bc8e7580baab101430e4bc
James Little <littleguy23@gmail.com>
2020-03-01 11:39:34 -0800

    Add Stork Formula

---
+++ Formula/stork.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Stork < Formula
+  desc "Impossibly fast web search, made for static sites."
+  homepage "https://stork-search.net"
+  url "https://github.com/jameslittle230/stork/releases/download/v0.5.0/stork-0.5.0-macos"
+  sha256 "16332a3d781f6bffba77cfe47b03823541ec1083355f5920e07b32ccc40af3da"
+
+  depends_on "rust" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test stork`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
+
