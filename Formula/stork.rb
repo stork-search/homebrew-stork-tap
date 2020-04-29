37edd296dea8d2571d6326beef734e6f798f30af
James Little <3058200+jameslittle230@users.noreply.github.com>
2020-04-28 17:39:51 -0700

    Update stork.rb

--- Formula/stork.rb
+++ Formula/stork.rb
@@ -1,29 +1,15 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Stork < Formula
   desc "Impossibly fast web search, made for static sites."
   homepage "https://stork-search.net"
-  url "https://github.com/jameslittle230/stork/archive/v0.5.0.tar.gz"
-  sha256 "2c395b0fba1d2f42e37590c01230c5bb8f4b1f1c77187f96d9c26da01123be0e"
+  url "https://github.com/jameslittle230/stork/archive/v0.6.0.tar.gz"
+  sha256 "81d46484e9d282483c03d969eb541df80b12d9eba8cae8ef7c9adff37ca2ea8b"
 
   depends_on "rust" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test stork`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
   end
 end
