7ced81bc28cc4561fe00403d216f3c312abda526
James Little <3058200+jameslittle230@users.noreply.github.com>
2021-04-21 19:29:47 -0700

    v1.2.0

--- Formula/stork.rb
+++ Formula/stork.rb
@@ -1,8 +1,8 @@
 class Stork < Formula
   desc "Impossibly fast web search, made for static sites."
   homepage "https://stork-search.net"
-  url "https://github.com/jameslittle230/stork/archive/v1.1.0.tar.gz"
-  sha256 "f951ebba14fe5b5902e60c650f3f478035bd3315286b8ebc68888b0f2bbc3203"
+  url "https://github.com/jameslittle230/stork/archive/v1.2.0.tar.gz"
+  sha256 "1fe2d8f37f5f0104faa04a92bbc76cb366f6c716b3c83ecb16af518b637f10be"
 
   depends_on "rust" => :build
 
